82902dc7934f196c234535176524b0886dff0314
Clynt Neiko Rupinta <neiko@meldcx.com>
2025-06-11 18:34:08 +0800

    Add unit testing project and update build configuration
    - Updated `MultiFormatDataConverter.sln` to include a new unit test project `MultiFormatDataConverter.UnitTest`.
    - Modified `Build.cs` to support cleaning, building, and testing the new unit test project, along with enhanced versioning and release note handling.
    - Created `MultiFormatDataConverter.UnitTest.csproj` with necessary configurations and package references.
    - Added a basic unit test class `UnitTest1.cs` using xUnit framework.

--- build/Build.cs
+++ build/Build.cs
@@ -1,8 +1,11 @@
+using Nuke.Common.Tools.DotNet;
 using NukeBuildHelpers;
 using NukeBuildHelpers.Common.Attributes;
+using NukeBuildHelpers.Common.Enums;
 using NukeBuildHelpers.Entry;
 using NukeBuildHelpers.Entry.Extensions;
 using NukeBuildHelpers.Runner.Abstraction;
+using NukeBuildHelpers.RunContext.Extensions;
 
 class Build : BaseNukeBuildHelpers
 {
@@ -12,30 +15,81 @@ class Build : BaseNukeBuildHelpers
 
     public override string MainEnvironmentBranch => "master";
 
+    [SecretVariable("NUGET_AUTH_TOKEN")]
+    readonly string? NuGetAuthToken;
+
     [SecretVariable("GITHUB_TOKEN")]
     readonly string? GithubToken;
 
-    BuildEntry BuildEntry => _ => _
-        .AppId("sample_app")
-        .RunnerOS(RunnerOS.Windows2022)
-        .Execute(() =>
-        {
-            // build logic here
-        });
-
-    TestEntry TestEntry => _ => _
-        .AppId("sample_app")
-        .RunnerOS(RunnerOS.Ubuntu2204)
-        .Execute(() =>
-        {
-            // test logic here
-        });
-
-    PublishEntry PublishEntry => _ => _
-        .AppId("sample_app")
+    BuildEntry MultiFormatDataConverterBuild => _ => _
+        .AppId("multi-format-data-converter")
         .RunnerOS(RunnerOS.Ubuntu2204)
         .Execute(context =>
         {
-            // publish logic here
+            var projectPath = RootDirectory / "MultiFormatDataConverter" / "MultiFormatDataConverter.csproj";
+            var version = "0.0.0";
+            var releaseNotes = "";
+            if (context.TryGetBumpContext(out var bumpContext))
+            {
+                version = bumpContext.AppVersion.Version.ToString();
+                releaseNotes = bumpContext.AppVersion.ReleaseNotes;
+            }
+            DotNetTasks.DotNetClean(_ => _
+                .SetProject(projectPath));
+            DotNetTasks.DotNetBuild(_ => _
+                .SetProjectFile(projectPath)
+                .SetConfiguration("Release"));
+            DotNetTasks.DotNetPack(_ => _
+                .SetProject(projectPath)
+                .SetConfiguration("Release")
+                .SetNoRestore(true)
+                .SetNoBuild(true)
+                .SetIncludeSymbols(true)
+                .SetSymbolPackageFormat("snupkg")
+                .SetVersion(version)
+                .SetPackageReleaseNotes(NormalizeReleaseNotes(releaseNotes))
+                .SetOutputDirectory(OutputDirectory));
         });
+
+    TestEntry MultiFormatDataConverterTest => _ => _
+        .AppId("multi-format-data-converter")
+        .Execute(context =>
+        {
+            var projectPath = RootDirectory / "MultiFormatDataConverter.UnitTest" / "MultiFormatDataConverter.UnitTest.csproj";
+            DotNetTasks.DotNetClean(_ => _
+                .SetProject(projectPath));
+            DotNetTasks.DotNetTest(_ => _
+                .SetProjectFile(projectPath));
+        })
+        .Matrix([new { Id = "LINUX", RunnerOS = RunnerOS.Ubuntu2204 }, new { Id = "WINDOWS", RunnerOS = RunnerOS.Windows2022 }],
+            (_, osMatrix) => _
+                .RunnerOS(osMatrix.RunnerOS)
+                .WorkflowId($"TEST_{osMatrix.Id}"));
+
+    PublishEntry MultiFormatDataConverterPublish => _ => _
+        .AppId("multi-format-data-converter")
+        .RunnerOS(RunnerOS.Ubuntu2204)
+        .ReleaseCommonAsset(OutputDirectory)
+        .Execute(context =>
+        {
+            if (context.RunType == RunType.Bump)
+            {
+                DotNetTasks.DotNetNuGetPush(_ => _
+                    .SetSource("https://nuget.pkg.github.com/kiryuumaru/index.json")
+                    .SetApiKey(GithubToken)
+                    .SetTargetPath(OutputDirectory / "**"));
+                DotNetTasks.DotNetNuGetPush(_ => _
+                    .SetSource("https://api.nuget.org/v3/index.json")
+                    .SetApiKey(NuGetAuthToken)
+                    .SetTargetPath(OutputDirectory / "**"));
+            }
+        });
+
+    private string? NormalizeReleaseNotes(string? releaseNotes)
+    {
+        return releaseNotes?
+            .Replace(",", "%2C")?
+            .Replace(":", "%3A")?
+            .Replace(";", "%3B");
+    }
 }
